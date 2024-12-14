7aa329982ae4139f77cf9774be6a6ca9e1e3efa8
1-Ujjwal <kingayushman7@gmail.com>
2024-12-14 23:11:34 +0530

    created Footer component
    -made Footer component and modified the font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Appointment from "./pages/Appointment";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import About from "./pages/About";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
         <Route path="/my-appointments" element={<MyAppointments />} />
         <Route path="/appointment/:docId" element={<Appointment />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
